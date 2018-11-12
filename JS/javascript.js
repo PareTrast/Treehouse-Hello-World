d1b2cde202686a90e99a33d2d1c5d3f1247cb09f
pare.and.trast <pare.and.trast@gmail.com>
2018-11-12 18:25:08 -0500

    message

---
+++ JS/javascript.js
@@ -0,0 +1,14 @@
+var encourage = [
+  "Keep on Pushing! Things will get better.  Trust me!",
+  "Do not let what you cannot do interfere with what you can do- John Wooden",
+  "Start by doing what's necessary; then do what is possible; and suddenly you are doing the impossible.- St. Francis of Assisi",
+  "The greatest discovery of my generation is that a human being can change his life by changing his attitude of mind.- William James",
+  "Many of life's failures are experienced by people who did not realize how close they were to success when they gave up.- Thomas Edison",
+  "Forget past mistakes.  Forget Failures.  Forget about everything except what you're going to do now and do it.- WIlliam Durant",
+];
+
+var randomQuote = encourage[Math.floor(Math.random()*(encourage.length))];
+console.log(randomQuote);
+$('button').click(function(){
+    alert(randomQuote);
+});
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-Hello World
+Ever since my mother passed away I have always had problems with anxiety and depression.  This simple random quote generator is a simple 
+project I put together to generate some encouraging quotes to help people.  Sometimes the best thing to help during an anxiety attack, or 
+in a depressive episode is to have some consoling words.  I may not be able to help the world, but if I can help just a few people then 
+I will have fulfilled my goal.
+
+After clicking the button you will recieve a random quote.  Refresh the page after each generation.
